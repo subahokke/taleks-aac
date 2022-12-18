f0a8b6267f8512979f3c9c51de37957bdb80d09b
subahokke <53539779+subahokke@users.noreply.github.com>
2022-12-18 00:09:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+let msg = new SpeechSynthesisUtterance();
+msg.lang = "en-US";
+msg.voice = speechSynthesis.getVoices().filter(function (voice) {
+  return voice.name == "Samantha";
+})[0];
 
+function speakMessage(clicked_id) {
+  msg.text = clicked_id;
+  window.speechSynthesis.speak(msg);
+}
 
-
-function speakMessage(clicked_id){
-    let msg = new SpeechSynthesisUtterance(clicked_id);
-    msg.lang = "en-US";
-    window.speechSynthesis.speak(msg);
-    }
-
-function submit(){
-    let inputText = document.getElementById("input").value;
-    msg.text = inputText;
-    window.speechSynthesis.speak(msg);
-    document.getElementById("input").value = '';
+function submit() {
+  let inputText = document.getElementById("input").value;
+  msg.text = inputText;
+  window.speechSynthesis.speak(msg);
+  document.getElementById("input").value = "";
 }
